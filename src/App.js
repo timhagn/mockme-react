cda8558d5d21a8f7b374a9f5c2d8f2e701198d22
timhagn <mail@timhagn.com>
2018-11-28 21:32:59 +0100

    add margin-bottom

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ import DropzoneWithPreview from "./DropzonePreview";
 class App extends Component {
   render() {
     return (
-      <div className="field--type-image field--name-field-image field--widget-image-image js-form-wrapper form-wrapper">
+      <div className="field--type-image field--name-field-image field--widget-image-image js-form-wrapper form-wrapper"
+           style={{
+             marginBottom: `2rem`,
+           }}>
           <DropzoneWithPreview />
       </div>
     );
