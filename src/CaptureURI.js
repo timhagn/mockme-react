f026abce8b75fc5a75d8984a9f6a093452092903
timhagn <mail@timhagn.com>
2018-11-28 20:55:04 +0100

    finish mm for the moment

--- src/CaptureURI.js
+++ src/CaptureURI.js
@@ -1,11 +1,12 @@
 import React from "react";
 
-// className="form-control"
-
 const CaptureURIInput = React.forwardRef((props, ref) => (
     <div className="js-form-item form-item js-form-type-textfield form-type-textfield">
-      <label htmlFor="url">URL</label>
-      <input ref={ref} type="url" className="js-text-full text-full form-text" name="url"
+      <label htmlFor="url">URL to snap</label>
+      <input ref={ref}
+             type="url"
+             className="js-text-full text-full form-text"
+             name="url"
              placeholder="URL to screenshot" />
       <input className="button js-form-submit form-submit"
              formNoValidate="formnovalidate"
