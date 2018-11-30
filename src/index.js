98d549cb474bd774a43d6f9bccd730a02a7269f4
timhagn <mail@timhagn.com>
2018-11-30 18:39:12 +0100

    try to save react nodes

--- src/index.js
+++ src/index.js
@@ -9,8 +9,12 @@ const drupalSettings = typeof window.drupalSettings !== 'undefined' ?
 const rootElement = drupalSettings.hasOwnProperty('mockmeRoot') ?
     drupalSettings.mockmeRoot : 'mockme-root'
 
-ReactDOM.render(<App drupalSettings={drupalSettings}/>, document.getElementById(rootElement))
+const reactNodes =
+    ReactDOM.render(<App drupalSettings={drupalSettings}/>, document.getElementById(rootElement))
 
+if (drupalSettings.hasOwnProperty('mockmeRoot')) {
+  drupalSettings.mockmeReact = reactNodes
+}
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
